3a7564a8eb8d6412f180d15d395d7ded41c002e2
Adam Becker <ad@mbecker.cc>
2022-06-17 18:57:17 -0700

    add example

--- src/Contract.sol
+++ src/Contract.sol
@@ -3,20 +3,57 @@ pragma solidity ^0.8.13;
 
 import "./interfaces/IVault.sol";
 import "forge-std/console.sol";
+import "openzeppelin-contracts/interfaces/IERC20.sol";
+
+contract FakeOracle {
+  int256 immutable fakeAnswer;
+
+  constructor (int256 _fakeAnswer) {
+    fakeAnswer = _fakeAnswer;
+  }
+
+	function latestRoundData() external view returns (uint80, int256 answer, uint256, uint256, uint80) {
+    return (0, fakeAnswer, 0, 0, 0);
+  }
+}
 
 contract Contract {
   address immutable owner;
+  address MAI_ADDRESS = 0xfB98B335551a418cD0737375a2ea0ded62Ea213b;
+  IVault vault = IVault(0x75D4aB6843593C111Eeb02Ff07055009c836A1EF);
 
   constructor() {
     owner = msg.sender;
+    IERC20(MAI_ADDRESS).approve(address(vault), type(uint).max);
   }
 
-  function execute() external {
+  function execute(uint vaultId, address _vaultOwnerAfterExecution) external {
     require(msg.sender == owner, "not owner");
 
-    uint vaultId = 48;
-    IVault vault = IVault(0x75D4aB6843593C111Eeb02Ff07055009c836A1EF);
+    uint newPrice = (vault.vaultDebt(vaultId) * 1e8 / vault.vaultCollateral(vaultId)) + 1;
+    FakeOracle fakeOracle = new FakeOracle(int(newPrice));
 
-    console.log(vault.checkLiquidation(vaultId));
+    uint initialGainRatio = vault.gainRatio();
+    address initialPriceSource = vault.ethPriceSource();
+    address initialStabilityPool = vault.stabilityPool();
+    uint initialClosingFee = vault.closingFee();
+    uint initialDebtRatio = vault.debtRatio();
+
+    vault.setGainRatio(1000);
+    vault.changeEthPriceSource(address(fakeOracle));
+    vault.setStabilityPool(address(0));
+    vault.setClosingFee(0);
+    vault.setDebtRatio(1);
+
+    vault.liquidateVault(vaultId);
+    vault.getPaid();
+
+    // reset initial values:
+    vault.setGainRatio(initialGainRatio);
+    vault.changeEthPriceSource(initialPriceSource);
+    vault.setStabilityPool(initialStabilityPool);
+    vault.setClosingFee(initialClosingFee);
+    vault.setDebtRatio(initialDebtRatio);
+    vault.transferOwnership(_vaultOwnerAfterExecution);
   }
 }
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,13 +2,47 @@
 pragma solidity ^0.8.13;
 
 import "../src/Contract.sol";
+import "../src/interfaces/IVault.sol";
 import "forge-std/Test.sol";
+import "openzeppelin-contracts/interfaces/IERC20.sol";
 
 contract ContractTest is Test {
-    function setUp() public {}
+  address MAI_WHALE_ADDRESS = 0x679016B3F8E98673f85c6F72567f22b58Aa15A54;
+  address MAI_ADDRESS = 0xfB98B335551a418cD0737375a2ea0ded62Ea213b;
+  address MOOBIFI_ADDRESS = 0xbF07093ccd6adFC3dEB259C557b61E94c1F66945;
+  address MOOBIFI_VAULT_ADDRESS = 0x75D4aB6843593C111Eeb02Ff07055009c836A1EF;
 
-    function testExample() public {
-      Contract instance = new Contract();
-      instance.execute();
-    }
+  function setUp() public {}
+
+  function testExample() public {
+    uint vaultId = 48;
+
+    IERC20 moobifi = IERC20(MOOBIFI_ADDRESS);
+
+    Contract instance = new Contract();
+    IVault vault = IVault(MOOBIFI_VAULT_ADDRESS);
+
+    // transfer mai from whale
+    vm.prank(MAI_WHALE_ADDRESS);
+    IERC20(MAI_ADDRESS).transfer(address(instance), 3000000 * 1e18);
+
+    console.log("start");
+    console.log("====");
+    console.log("vault collateral", vault.vaultCollateral(vaultId));
+    console.log("vault debt", vault.vaultDebt(vaultId));
+    console.log("");
+    console.log("");
+
+    address originalOwner = vault.owner();
+    vm.prank(originalOwner);
+    vault.transferOwnership(address(instance));
+
+    instance.execute(vaultId, originalOwner);
+
+    console.log("end");
+    console.log("====");
+    console.log("vault collateral", vault.vaultCollateral(vaultId));
+    console.log("vault debt", vault.vaultDebt(vaultId));
+    console.log("liquidated moobifi", moobifi.balanceOf(address(instance)));
+  }
 }
