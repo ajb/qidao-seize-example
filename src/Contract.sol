940b7893301240a94546cb1e847bc2f4ce1e1ddd
Adam Becker <ad@mbecker.cc>
2022-06-17 18:01:13 -0700

    init

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {}
+import "./interfaces/IVault.sol";
+import "forge-std/console.sol";
+
+contract Contract {
+  address immutable owner;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  function execute() external {
+    require(msg.sender == owner, "not owner");
+
+    uint vaultId = 48;
+    IVault vault = IVault(0x75D4aB6843593C111Eeb02Ff07055009c836A1EF);
+
+    console.log(vault.checkLiquidation(vaultId));
+  }
+}
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "../src/Contract.sol";
 import "forge-std/Test.sol";
 
 contract ContractTest is Test {
     function setUp() public {}
 
     function testExample() public {
-        assertTrue(true);
+      Contract instance = new Contract();
+      instance.execute();
     }
 }
